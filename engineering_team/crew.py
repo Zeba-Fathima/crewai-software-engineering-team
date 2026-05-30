0f46857604f8ce4d2e4914af6d6d40af8bf36863
Zeba-Fathima <121533770+Zeba-Fathima@users.noreply.github.com>
2026-05-30 18:11:49 +0530

    Update crew.py

--- engineering_team/crew.py
+++ engineering_team/crew.py
@@ -77,6 +77,5 @@ class EngineeringTeam():
             agents=self.agents,
             tasks=self.tasks,
             process=Process.sequential,
-            verbose=True,
-            tracing=True
+            verbose=True
         )
